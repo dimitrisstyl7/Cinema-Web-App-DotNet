a3856d5df8b0511f34d3f45cbd1e66f54a9ccb84
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-01-18 18:08:29 +0200

    first version of login page

---
+++ CinemaWebApp/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using CinemaWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        private readonly CinemaAppDBContext _context;
+
+        public AccountController(CinemaAppDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login([Bind("Email, Password")] User user)
+        {
+            var db_user = await _context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
+
+            if (db_user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            bool isPasswordValid = BCrypt.Net.BCrypt.Verify(user.Password, db_user.Password);
+
+            if (!isPasswordValid)
+            {
+                return NotFound("Invalid password");
+            }
+
+            return RedirectToAction("Index", "Privacy");
+        }
+
+    }
+}
