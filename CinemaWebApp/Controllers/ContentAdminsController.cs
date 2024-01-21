3e9b74bf541cc407a34f7d792f1283b00d42eb48
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2024-01-22 01:40:48 +0200

    Show all available movies based on ContentAdminId and Insert a new movie, Edit, Show Details, Delete a movie based on ContentAdminId

---
+++ CinemaWebApp/Controllers/ContentAdminsController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CinemaWebApp.Models;
+
+namespace CinemaWebApp.Controllers
+{
+    public class ContentAdminsController : Controller
+    {
+        private readonly CinemaAppDBContext _context;
+
+        public ContentAdminsController(CinemaAppDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ContentAdmins/Id
+        public async Task<IActionResult> Index(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            
+            var movies = await _context.Movies
+                .Include(m => m.Genre)
+                .Where(m => m.ContentAdminId == id)
+                .ToListAsync();
+            ViewData["ContentAdminId"] = id;
+            return View(movies);
+        }
+
+        // GET: ContentAdmins/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contentAdmin = await _context.ContentAdmins
+                .Include(c => c.Cinema)
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contentAdmin == null)
+            {
+                return NotFound();
+            }
+
+            return View(contentAdmin);
+        }
+
+        // GET: ContentAdmins/Create
+        public IActionResult Create()
+        {
+            ViewData["CinemaId"] = new SelectList(_context.Cinemas, "Id", "Id");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: ContentAdmins/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,CinemaId")] ContentAdmin contentAdmin)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contentAdmin);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CinemaId"] = new SelectList(_context.Cinemas, "Id", "Id", contentAdmin.CinemaId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", contentAdmin.UserId);
+            return View(contentAdmin);
+        }
+
+        // GET: ContentAdmins/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contentAdmin = await _context.ContentAdmins.FindAsync(id);
+            if (contentAdmin == null)
+            {
+                return NotFound();
+            }
+            ViewData["CinemaId"] = new SelectList(_context.Cinemas, "Id", "Id", contentAdmin.CinemaId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", contentAdmin.UserId);
+            return View(contentAdmin);
+        }
+
+        // POST: ContentAdmins/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,CinemaId")] ContentAdmin contentAdmin)
+        {
+            if (id != contentAdmin.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contentAdmin);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContentAdminExists(contentAdmin.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CinemaId"] = new SelectList(_context.Cinemas, "Id", "Id", contentAdmin.CinemaId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", contentAdmin.UserId);
+            return View(contentAdmin);
+        }
+
+        // GET: ContentAdmins/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contentAdmin = await _context.ContentAdmins
+                .Include(c => c.Cinema)
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contentAdmin == null)
+            {
+                return NotFound();
+            }
+
+            return View(contentAdmin);
+        }
+
+        // POST: ContentAdmins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contentAdmin = await _context.ContentAdmins.FindAsync(id);
+            if (contentAdmin != null)
+            {
+                _context.ContentAdmins.Remove(contentAdmin);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContentAdminExists(int id)
+        {
+            return _context.ContentAdmins.Any(e => e.Id == id);
+        }
+    }
+}
