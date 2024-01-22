6db848e7eed13217237e6e84013ac883c5c0f751
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-22 21:17:07 +0200

    Create application admin controller with the corresponding html pages #8

---
+++ CinemaWebApp/Controllers/AppAdminsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CinemaWebApp.Models;
+
+namespace CinemaWebApp.Controllers
+{
+    public class AppAdminsController : Controller
+    {
+        private readonly CinemaAppDBContext _context;
+
+        public AppAdminsController(CinemaAppDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AppAdmins/Index/{AppAdminId}
+        public async Task<IActionResult> Index(int? id)
+        {
+            var contentAdmins = await _context.ContentAdmins
+                .Include(admin => admin.User)
+                .GroupBy(admin => admin.Cinema)
+                .ToListAsync();
+
+            if (contentAdmins.Count == 0)
+            {
+                ViewData["Message"] = "No content admins found.";
+            }
+
+            ViewData["AppAdminId"] = id;
+            return View(contentAdmins);
+        }
+    }
+}
