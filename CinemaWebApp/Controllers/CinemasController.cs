f5228eda400dbc3b7bd9f800ca7f77698be9f0d0
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-25 18:03:00 +0200

    Update CinemasController.cs

--- CinemaWebApp/Controllers/CinemasController.cs
+++ CinemaWebApp/Controllers/CinemasController.cs
@@ -13,13 +13,7 @@ namespace CinemaWebApp.Controllers
             _context = context;
         }
 
-        // GET: Cinemas
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Cinemas.ToListAsync());
-        }
-
-        // GET: Cinemas/Details/5
+        // GET: Cinemas/Details/{CinemaId}
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -49,117 +43,6 @@ namespace CinemaWebApp.Controllers
             }
 
             return View(cinemaScreenings);
-        }
-
-        // GET: Cinemas/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Cinemas/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Address,City,ZipCode,Email,NoOfScreeningRooms")] Cinema cinema)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(cinema);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(cinema);
-        }
-
-        // GET: Cinemas/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cinema = await _context.Cinemas.FindAsync(id);
-            if (cinema == null)
-            {
-                return NotFound();
-            }
-            return View(cinema);
-        }
-
-        // POST: Cinemas/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Address,City,ZipCode,Email,NoOfScreeningRooms")] Cinema cinema)
-        {
-            if (id != cinema.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(cinema);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CinemaExists(cinema.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(cinema);
-        }
-
-        // GET: Cinemas/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cinema = await _context.Cinemas
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (cinema == null)
-            {
-                return NotFound();
-            }
-
-            return View(cinema);
-        }
-
-        // POST: Cinemas/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var cinema = await _context.Cinemas.FindAsync(id);
-            if (cinema != null)
-            {
-                _context.Cinemas.Remove(cinema);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool CinemaExists(int id)
-        {
-            return _context.Cinemas.Any(e => e.Id == id);
         }
     }
 }
